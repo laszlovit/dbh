003e8335291680d5e0e6b43f444a863f05742382
laszlovit <laszlo.vitkai@gmail.com>
2025-01-15 13:16:59 +0100

    setup staging and production environments

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,21 +10,42 @@ import { media } from "sanity-plugin-media";
 import { structureTool } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from "./src/sanity/env";
+import { CodeBracketSquareIcon, RocketLaunchIcon } from "@heroicons/react/16/solid";
+import { apiVersion, projectId } from "./src/sanity/env";
 import { schema } from "./src/sanity/schema";
 import { structure } from "./src/sanity/structure";
 
-export default defineConfig({
-	basePath: "/studio",
-	projectId,
-	dataset,
-	// Add and edit the content schema in the './sanity/schemaTypes' folder
-	schema,
-	plugins: [
-		structureTool({ structure }),
-		// Vision is for querying with GROQ from inside the Studio
-		// https://www.sanity.io/docs/the-vision-plugin
-		visionTool({ defaultApiVersion: apiVersion }),
-		media(),
-	],
-});
+export default defineConfig([
+	{
+		name: "production",
+		basePath: "/studio",
+		icon: RocketLaunchIcon,
+		projectId,
+		dataset: "production",
+		// Add and edit the content schema in the './sanity/schemaTypes' folder
+		schema,
+		plugins: [
+			structureTool({ structure }),
+			// Vision is for querying with GROQ from inside the Studio
+			// https://www.sanity.io/docs/the-vision-plugin
+			visionTool({ defaultApiVersion: apiVersion }),
+			media(),
+		],
+	},
+	{
+		name: "staging",
+		basePath: "/staging",
+		icon: CodeBracketSquareIcon,
+		projectId,
+		dataset: "staging",
+		// Add and edit the content schema in the './sanity/schemaTypes' folder
+		schema,
+		plugins: [
+			structureTool({ structure }),
+			// Vision is for querying with GROQ from inside the Studio
+			// https://www.sanity.io/docs/the-vision-plugin
+			visionTool({ defaultApiVersion: apiVersion }),
+			media(),
+		],
+	},
+]);
