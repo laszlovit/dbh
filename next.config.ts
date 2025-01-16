39a44c24779d1fe155f9079ce6fed1d8cc6f6695
laszlovit <laszlo.vitkai@gmail.com>
2025-01-16 18:16:31 +0100

    added redirects before migration process

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,35 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
+	async redirects() {
+		return [
+			{
+				source: "/vinduespudsning",
+				destination: "/services/vinduespolering",
+				permanent: false,
+			},
+			{
+				source: "/kopi-af-vinduespudsning-privat",
+				destination: "/services/fliserens",
+				permanent: true,
+			},
+			{
+				source: "/kopi-af-flise-og-facaderens",
+				destination: "/services/rens-af-tagrender",
+				permanent: true,
+			},
+			{
+				source: "/vinduespudsning-erhverv",
+				destination: "/services/vinduespolering",
+				permanent: false,
+			},
+			{
+				source: "/kundeservice",
+				destination: "/kontakt",
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default nextConfig;
